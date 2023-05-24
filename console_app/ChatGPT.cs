0e7dec392a5e8d47ab3ce62ad78edb2a3875011b
Jackson-Wheeler <jackson.n.wheeler@gmail.com>
2023-05-23 17:39:48 -0700

    initial commit

---
+++ console_app/ChatGPT.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Contains class for interacting with ChatGPT API
+ */
+
+using System;
+using System.Configuration;
+using OpenAI_API;
+using OpenAI_API.Chat;
+
+namespace console_app;
+
+public class ChatGPT
+{
+    private const string API_KEY_NAME = "apiKey";
+    private string? api_key;
+    private OpenAIAPI api;
+    private Conversation chat;
+
+    public ChatGPT()
+    {
+        // Reads api_key from App.config file
+        api_key = ConfigurationManager.AppSettings.Get(API_KEY_NAME);
+        api = new OpenAIAPI(new APIAuthentication(api_key));
+        chat = api.Chat.CreateConversation();
+    }
+
+    internal async Task askQuestion(string prompt)
+    {
+        chat.AppendUserInput(prompt);
+        string response = await chat.GetResponseFromChatbotAsync();
+        Console.Write("Response: ");
+        Console.WriteLine(response);
+    }
+
+
+    internal async Task doSampleChat() {
+        /// give instruction as System
+        chat.AppendSystemMessage("You are a teacher who helps children understand if things are animals or not.  If the user tells you an animal, you say \"yes\".  If the user tells you something that is not an animal, you say \"no\".  You only ever respond with \"yes\" or \"no\".  You do not say anything else.");
+
+        // give a few examples as user and assistant
+        chat.AppendUserInput("Is this an animal? Cat");
+        chat.AppendExampleChatbotOutput("Yes");
+        chat.AppendUserInput("Is this an animal? House");
+        chat.AppendExampleChatbotOutput("No");
+
+        // now let's ask it a question'
+        chat.AppendUserInput("Is this an animal? Dog");
+        // and get the response
+        var response = await chat.GetResponseFromChatbotAsync();
+        Console.WriteLine(response); // "Yes"
+
+        // and continue the conversation by asking another
+        chat.AppendUserInput("Is this an animal? Chair");
+        // and get another response
+        response = await chat.GetResponseFromChatbotAsync();
+        Console.WriteLine(response); // "No"
+
+        // the entire chat history is available in chat.Messages
+        foreach (ChatMessage msg in chat.Messages)
+        {
+            Console.WriteLine($"{msg.Role}: {msg.Content}");
+        }
+
+        //return true;
+
+    }
+    
+}
+
---
+++ console_app/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace console_app;
+
+internal class Program
+{
+    private const string QUIT_KEYWORD = "quit";
+
+    // Main Method
+    static async Task Main(string[] args)
+    {
+        consoleIntro();
+        ChatGPT chat = new ChatGPT();
+
+        // Main loop - Ask question, read user inputted question, and submit to ChatGPT
+        while (true)
+        {
+            Console.Write("\nWrite Your Question: ");
+            string? prompt = Console.ReadLine();
+            if (prompt == null) continue;
+            if (prompt == QUIT_KEYWORD) break; // logic to exit loop
+            // Ask question through ChatGPT api & print result
+            await chat.askQuestion(prompt);
+        }
+
+        finish();
+    }
+
+    // Print intro information to console for user
+    public static void consoleIntro()
+    {
+        Console.WriteLine("Hello there!");
+        Console.WriteLine("Welcome to a Console version of ChatGPT! Ask away!");
+        Console.WriteLine($"Type '{QUIT_KEYWORD}' and press Enter to quit program");
+        Console.WriteLine();
+    }
+
+    // End program
+    public static void finish()
+    {
+        Console.WriteLine();
+        Console.Write($"{Environment.NewLine}Press any key to exit...");
+        Console.ReadKey(true);
+    }
+
+    // Code to test console output
+    public static void askName()
+    {
+        Console.Write("Enter your name: ");
+        string? name = Console.ReadLine();
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}. I hope you are doing well today {currentDate:d} at {currentDate:t}");
+    }
+
+}
